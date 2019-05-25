879d3c3e4cb10e26c90b0f0ee3d4d0df60a7b013
Marco Mariani <marco.mariani619@gail.com>
2019-05-24 18:21:00 -0700

    Psychic HW

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,28 @@
-console.log("hello")
+// Create an array for the entire alphabet
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+// Next couple of lines come from the RPS game solved code
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    var computerGuess = computerChoices[alphabet.arr(alphabet.random() * alphabet.length)];
+}
+
+// These variables, which I found on stack make sense to me to store as reference moving forward
+var wins = 0;
+var losses = 0;
+var left = 9;
+var guesses = 9;
+var guessesSoFar = [];
+var psychicLetter; //?
+
+// The psychic letter function runs everytime the correct userGuess is pressed by randomly generating from the array.
+
+
+// Create a wins counter every time user clicks the correct key
+
+// Create a losses counter every time user does not guess correctly within 9 key strokes
+
+// Create a count down counter starting from 9, which goes down by 1 every time user strokes incorrect key
+
+// Show on screen the different key strokes for a single "game" of 9 key strokes
+
